5c984a442656fa658c93b03bdaee35b9ad1e8ddf
Luiz Brancher <luiz.pb@cryptolab.net>
2022-07-13 16:07:43 -0300

    alteracoes lp

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$('#btn-1').hover(function (){
+    $('#btn-1').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-1').css('transform', 'scale(1)')
+})
+
+$('#btn-2').hover(function (){
+    $('#btn-2').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-2').css('transform', 'scale(1)')
+})
+
+$('#btn-3').hover(function (){
+    $('#btn-3').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-3').css('transform', 'scale(1)')
+})
+
+$('#btn-4').hover(function (){
+    $('#btn-4').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-4').css('transform', 'scale(1)')
+})
+
+$('#btn-5').hover(function (){
+    $('#btn-5').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-5').css('transform', 'scale(1)')
+})
+$('#btn-6').hover(function (){
+    $('#btn-6').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-6').css('transform', 'scale(1)')
+})
+$('#btn-7').hover(function (){
+    $('#btn-7').css('transform', 'scale(3)')
+}, function (){
+    $('#btn-7').css('transform', 'scale(1)')
+})
