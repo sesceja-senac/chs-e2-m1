cf851465d1b61d6969e167a2d9306d9b9b314550
Luiz Brancher <lppbrancher@senacrs.com.br>
2022-07-19 02:25:07 -0300

    layout pg 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,7 @@
-$('#btn-1').hover(function (){
-    $('#btn-1').css('transform', 'scale(3)')
+$('.painel-container button').hover(function (){
+    $(this).css('transform', 'scale(2)')
 }, function (){
-    $('#btn-1').css('transform', 'scale(1)')
-})
-
-$('#btn-2').hover(function (){
-    $('#btn-2').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-2').css('transform', 'scale(1)')
-})
-
-$('#btn-3').hover(function (){
-    $('#btn-3').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-3').css('transform', 'scale(1)')
-})
-
-$('#btn-4').hover(function (){
-    $('#btn-4').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-4').css('transform', 'scale(1)')
-})
-
-$('#btn-5').hover(function (){
-    $('#btn-5').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-5').css('transform', 'scale(1)')
-})
-$('#btn-6').hover(function (){
-    $('#btn-6').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-6').css('transform', 'scale(1)')
-})
-$('#btn-7').hover(function (){
-    $('#btn-7').css('transform', 'scale(3)')
-}, function (){
-    $('#btn-7').css('transform', 'scale(1)')
+    $(this).css('transform', 'scale(1)')
 })
 
 $('.card-sesc').click(function(){
