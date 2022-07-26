9ba17d79cf5c382264b39df71b36782816f88433
Luiz Brancher <lppbrancher@senacrs.com.br>
2022-07-25 21:28:57 -0300

    tela inicial

--- assets/js/tela_inicial.js
+++ assets/js/tela_inicial.js
@@ -4,13 +4,13 @@ window.setInterval(function(){
     if(bg==1){
         $('.background-1').fadeOut(2000)
         $('.background-2').fadeIn(2000)
-        $('.menu-container').removeClass('darker')
+        $('.container').addClass('noite')
         bg=2
         console.log(bg)
     } else {
         $('.background-2').fadeOut(2000)
         $('.background-1').fadeIn(2000)
-        $('.menu-container').addClass('darker')
+        $('.container').removeClass('noite')        
         bg=1
         console.log(bg)
     }
